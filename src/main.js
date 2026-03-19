9678d449b87be5799356136beb6fdbf4924a42b0
k0tox <kazua01fn@outlook.com>
2026-03-18 21:27:14 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import { Engine } from './core/Engine.js';
 import { Input } from './core/Input.js';
 import { Time } from './core/Time.js';
+import { MouseLook } from './core/MouseLook.js';
 
 import { Ocean } from './world/Ocean.js';
 import { Sky } from './world/Sky.js';
 
 import { Player } from './player/Player.js';
+import { CameraController } from './player/CameraController.js';
 
+import { Rod } from './fishing/Rod.js';
+import { Line } from './fishing/Line.js';
+import { FishManager } from './fishing/FishManager.js';
+import { HookDetector } from './fishing/HookDetector.js';
 import { CatchSystem } from './fishing/CatchSystem.js';
 
 import { HUD } from './ui/HUD.js';
@@ -19,37 +25,58 @@ const time = new Time();
 // World
 const ocean = new Ocean();
 engine.add(ocean.mesh);
-
 const sky = new Sky(engine.scene);
 
-// Player
+// Player + camera
 const player = new Player(engine.camera, input);
+const mouseLook = new MouseLook(engine.camera);
+const cameraController = new CameraController(player, mouseLook);
 
 // Fishing
+const rod = new Rod(engine.camera);
+const line = new Line(engine.scene, rod);
+const fishManager = new FishManager(engine.scene, 15);
+const hookDetector = new HookDetector(rod, fishManager);
 const catchSystem = new CatchSystem();
 
 // UI
 const hud = new HUD();
 const inventory = new InventoryUI();
 
-// Update loop
+// Loop
 engine.register((delta) => {
   time.update(delta);
 
   ocean.update(delta);
   sky.update(delta);
-  player.update(delta);
+
+  cameraController.update(delta);
+
+  rod.update(delta);
+  line.update();
+
+  fishManager.update(delta);
   catchSystem.update(delta);
 
+  // CAST
   if (input.mouse.down) {
+    rod.cast();
     catchSystem.cast();
   }
 
+  // CHECK HOOK
+  const fish = hookDetector.check();
+  if (fish && catchSystem.state === "waiting") {
+    catchSystem.state = "bite";
+    hud.setText("BITE! Press SPACE!");
+  }
+
+  // REEL
   if (input.isKeyDown("Space")) {
-    const fish = catchSystem.reel();
-    if (fish) {
-      inventory.add(fish);
-      hud.setText(`Caught: ${fish.name}`);
+    const caught = catchSystem.reel();
+    if (caught) {
+      inventory.add(caught);
+      hud.setText(`Caught: ${caught.name}`);
     }
   }
 });
